70071a7b1264699965df893327f02573d24e8c0a
hancong03 <hancong03@baidu.com>
2014-07-26 18:39:06 +0800

    first version
    Signed-off-by: hancong03 <hancong03@baidu.com>

---
+++ plugins/formula/dialogs/formula.js
@@ -0,0 +1,81 @@
+﻿/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+( function() {
+
+    var pluginName = 'formula';
+
+	var formulaDialog = function( editor, dialogType ) {
+
+        var inited = false;
+
+        function insertFormula ( info ) {
+
+            var ele = editor.document.createElement( 'img' );
+            ele.setAttribute( 'src', info.img );
+
+            editor.insertElement( ele );
+
+        }
+
+        return {
+				title: '公式',
+				width: 780,
+				height: 500,
+                resizable: CKEDITOR.DIALOG_RESIZE_NONE,
+				onShow: function() {
+
+                    if ( !inited ) {
+                        inited = true;
+                        this.getElement().addClass( "kity-formula-dialog" );
+                    } else if ( window.kfEditor ) {
+//                        window.setTimeout( function () {
+//                            kfEditor.execCommand( "focus" );
+//                        }, 1000 );
+                    }
+
+				},
+				onOk: function() {
+
+                    if ( window.kfEditor ) {
+                        kfEditor.execCommand( "get.image.data", insertFormula );
+                    }
+
+				},
+                onCancel: function () {
+
+                    if ( window.kfEditor ) {
+                        kfEditor.execCommand( "render", "\\placeholder" );
+                    }
+
+                },
+				contents: [
+					{
+                        id: 'viewEdit',
+                        label: '可视化编辑',
+                        accessKey: 'F',
+                        elements: [ {
+                            type: 'html',
+                            html: '<div style="width: 780px; height: 500px;"><iframe style="width: 100%;height: 100%;" frameborder="0" src="../plugins/formula/page/index.html"></iframe></div>'
+                        } ]
+                    },
+					{
+                        id: 'latexEdit',
+                        label: 'LaTeX编辑',
+                        elements: [ {
+                            type: 'html',
+                            html: '<div id="formula"></div>'
+                        } ]
+                    }
+				]
+        };
+
+    };
+
+	CKEDITOR.dialog.add( pluginName, function( editor ) {
+		return formulaDialog( editor, pluginName );
+	} );
+
+} )();
---
+++ plugins/formula/plugin.js
@@ -0,0 +1,64 @@
+/**
+ * Kity Formula Plugin
+ */
+
+( function () {
+
+    var pluginName = 'formula',
+        commandName = 'insertformula';
+
+    CKEDITOR.plugins.add( pluginName, {
+        requires: 'dialog',
+        icons: 'formula', // %REMOVE_LINE_CORE%
+        hidpi: true, // %REMOVE_LINE_CORE%
+        init: function( editor ) {
+            // Abort when Image2 is to be loaded since both plugins
+            // share the same button, command, etc. names (#11222).
+            // Register the dialog.
+            CKEDITOR.dialog.add( pluginName, this.path + 'dialogs/formula.js' );
+//
+//            var allowed = 'img[alt,!src]{border-style,border-width,float,height,margin,margin-bottom,margin-left,margin-right,margin-top,width}',
+//                required = 'img[alt,src]';
+//
+//            if ( CKEDITOR.dialog.isTabEnabled( editor, pluginName, 'advanced' ) )
+//                allowed = 'img[alt,dir,id,lang,longdesc,!src,title]{*}(*)';
+
+            // Register the command.
+            editor.addCommand( pluginName, new CKEDITOR.dialogCommand( pluginName ) );
+
+            // Register the toolbar button.
+            editor.ui.addButton && editor.ui.addButton( 'Formula', {
+                label: '公式',
+                command: pluginName,
+                toolbar: 'insert'
+            } );
+
+//            editor.on( 'doubleclick', function( evt ) {
+//                var element = evt.data.element;
+//
+//                if ( element.is( 'img' ) && !element.data( 'cke-realelement' ) && !element.isReadOnly() )
+//                    evt.data.dialog = 'image';
+//            } );
+//
+//            // If the "menu" plugin is loaded, register the menu items.
+//            if ( editor.addMenuItems ) {
+//                editor.addMenuItems( {
+//                    image: {
+//                        label: editor.lang.image.menu,
+//                        command: 'image',
+//                        group: 'image'
+//                    }
+//                } );
+//            }
+
+            // If the "contextmenu" plugin is loaded, register the listeners.
+//            if ( editor.contextMenu ) {
+//                editor.contextMenu.addListener( function( element, selection ) {
+//                    if ( getSelectedImage( editor, element ) )
+//                        return { image: CKEDITOR.TRISTATE_OFF };
+//                } );
+//            }
+        }
+    } );
+
+} )();
