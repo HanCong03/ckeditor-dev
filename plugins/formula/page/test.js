3f5645238d4e7a39a0637dc4fa042adc7e3686d5
hancong03 <hancong03@baidu.com>
2014-08-22 01:58:21 +0800

    新版本UI
    Signed-off-by: hancong03 <hancong03@baidu.com>

---
+++ plugins/formula/page/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by hn on 14-8-7.
+ */
+
+
+var source = "a+b",
+    url = "http://localhost/ckeditor-dev/plugins/formula/page/mathjax.html";
+
+
+var html = '<iframe class="kf-formula-expression" contenteditable="false" frameborder="0" style="vertical-align: middle;" data-source="asdfasdf" src=""></iframe>';
